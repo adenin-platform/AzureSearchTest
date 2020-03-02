80c7dce73ce2370d33f7c3395452db49ead8052b
olliejm <olliejm@protonmail.com>
2020-03-02 11:27:03 +0000

    Initial commit

---
+++ AzureSearchTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using adenin.IntentRecognizer.Store.Models;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+
+namespace AzureSearchTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SearchIndexClient indexClient = new SearchIndexClient("adenin-intents", "utterances", new SearchCredentials("INDEX_QUERY_KEY"));
+
+            SearchParameters parameters = new SearchParameters
+            {
+                SearchMode = SearchMode.All,
+                QueryType = QueryType.Full
+            };
+
+            string searchText = "id:1=PTO-STATUS=my_PTO";
+
+            Console.WriteLine($"Executing query 'searchMode={parameters.SearchMode}&queryType={parameters.QueryType}&search={searchText}'\n");
+
+            // Repeat the attempt 5 times
+            for (int i = 0; i < 5; i++)
+            {
+                DocumentSearchResult<Utterance> searchResults = indexClient.Documents.Search<Utterance>(searchText, parameters);
+
+                Console.WriteLine($"Attempt {i + 1} returned {searchResults.Results.Count} results:");
+
+                foreach (SearchResult<Utterance> result in searchResults.Results)
+                {
+                    Console.WriteLine($"Utterance id: {result.Document.Id}");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ AzureSearchTest/Utterance.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+
+namespace adenin.IntentRecognizer.Store.Models
+{
+    [SerializePropertyNamesAsCamelCase]
+    public sealed class Utterance
+    {
+        public Utterance(string tenantId, string intentId, string entityType, string container, string content, string json)
+        {
+            Id = string.Join("=", tenantId, intentId, Regex.Replace(content, "[^A-Za-z0-9]", "_"));
+            TenantId = tenantId;
+            IntentId = intentId;
+            EntityType = entityType;
+            Container = container;
+            Content = content;
+            Json = json;
+        }
+
+        [System.ComponentModel.DataAnnotations.Key, IsSearchable]
+        public string Id { get; }
+
+        [IsSearchable]
+        public string TenantId { get; }
+
+        [IsSearchable]
+        public string IntentId { get; }
+
+        [IsSearchable]
+        public string EntityType { get; }
+
+        [IsSearchable]
+        public string Container { get; }
+
+        [IsSearchable]
+        public string Content { get; }
+
+        public string Json { get; }
+    }
+}
