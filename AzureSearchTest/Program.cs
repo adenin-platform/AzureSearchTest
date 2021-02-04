66d704a1d22d7f05a5f00bb61e4e4213eca70977
olliejm <olliejm@protonmail.com>
2021-02-04 10:17:39 +0000

    repro for 2nd azure search issue

--- AzureSearchTest/Program.cs
+++ AzureSearchTest/Program.cs
@@ -2,38 +2,114 @@
 using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AzureSearchTest
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            SearchIndexClient indexClient = new SearchIndexClient("adenin-intents", "utterances", new SearchCredentials(args[0]));
+            Console.WriteLine("Enter query API key: ");
 
-            SearchParameters parameters = new SearchParameters
+            var key = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(key))
+            {
+                Console.WriteLine("No key provided, exiting");
+            }
+
+            var indexClient = new SearchIndexClient("catalog-prod", "app", new SearchCredentials(key));
+
+            var parameters = new SearchParameters
             {
                 SearchMode = SearchMode.All,
                 QueryType = QueryType.Full
             };
 
-            string searchText = "id:1=PTO-STATUS=my_PTO";
+            bool retry = true;
 
-            Console.WriteLine($"Executing query 'searchMode={parameters.SearchMode}&queryType={parameters.QueryType}&search={searchText}'\n");
-
-            // Repeat the attempt 5 times
-            for (int i = 0; i < 5; i++)
+            while (retry)
             {
-                DocumentSearchResult<Utterance> searchResults = indexClient.Documents.Search<Utterance>(searchText, parameters);
+                Console.WriteLine("Enter tenant ID:");
 
-                Console.WriteLine($"Attempt {i + 1} returned {searchResults.Results.Count} results:");
+                var id = Console.ReadLine();
+                var searchText = $"tenantId:{id}";
 
-                foreach (SearchResult<Utterance> result in searchResults.Results)
+                Console.WriteLine($"Executing query 'searchMode={parameters.SearchMode}&queryType={parameters.QueryType}&search={searchText}'\n");
+
+                var count = 0;
+                var deduplicatedCount = 0;
+                var hadMismatch = false;
+
+                // Repeat the attempt 5 times
+                for (int i = 0; i < 50; i++)
                 {
-                    Console.WriteLine($"Utterance id: {result.Document.Id}");
+                    var searchResult = await indexClient.Documents.SearchAsync<Utterance>(searchText, parameters);
+                    var results = searchResult.Results as IEnumerable<SearchResult<Utterance>>;
+
+                    while (searchResult.ContinuationToken != null)
+                    {
+                        searchResult = await indexClient.Documents.ContinueSearchAsync<Utterance>(searchResult.ContinuationToken);
+                        results = results.Concat(searchResult.Results);
+                    }
+
+                    Console.WriteLine($"{i + 1}: raw results count: {results.Count()}");
+
+                    var deduplicatedResults = new List<Utterance>();
+
+                    foreach (var result in results)
+                    {
+                        if (!deduplicatedResults.Exists(u => u.IntentId == result.Document.IntentId))
+                        {
+                            deduplicatedResults.Add(result.Document);
+                        }
+                    }
+
+                    if (i == 0)
+                    {
+                        count = results.Count();
+                        deduplicatedCount = deduplicatedResults.Count();
+                    }
+
+                    var statement = "Previous";
+
+                    if (i == 1)
+                    {
+                        statement = "Initial";
+                    }
+
+                    if (count != results.Count())
+                    {
+                        hadMismatch = true;
+                        Console.WriteLine($"{i + 1}: count mismatch. {statement}: {count}, current: {results.Count()}");
+                    }
+
+                    if (deduplicatedCount != deduplicatedResults.Count())
+                    {
+                        hadMismatch = true;
+                        Console.WriteLine($"{i + 1}: deduplicated count mismatch. {statement}: {deduplicatedCount}, current: {deduplicatedResults.Count()}");
+                    }
+
+                    count = results.Count();
+                    deduplicatedCount = deduplicatedResults.Count();
                 }
 
-                Console.WriteLine("\n");
+                if (!hadMismatch)
+                {
+                    Console.WriteLine($"No mismatch was found with tenant '{id}'");
+                }
+
+                Console.WriteLine("\nTry another tenant? (y/n)");
+
+                var repeat = Console.ReadLine();
+
+                if (repeat != "y")
+                {
+                    retry = false;
+                }
             }
         }
     }
